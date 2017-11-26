f187398e21fe78ee90398ed709307c23f02c60d3
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-11-26 08:16:48 +0100

    Rate limit the reconnects

--- src/main/java/org/achfrag/crypto/bitfinex/HeartbeatThread.java
+++ src/main/java/org/achfrag/crypto/bitfinex/HeartbeatThread.java
@@ -1,5 +1,7 @@
 package org.achfrag.crypto.bitfinex;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.achfrag.crypto.bitfinex.commands.PingCommand;
@@ -12,12 +14,20 @@ class HeartbeatThread extends ExceptionSafeThread {
 	 */
 	private static final long TIMEOUT = TimeUnit.SECONDS.toMillis(30);
 	
-	
 	/**
 	 * The API timeout
 	 */
 	private static final long HEARTBEAT = TimeUnit.SECONDS.toMillis(5);
 	
+	/**
+	 * Max reconnects
+	 */
+	private static final int MAX_RECONNECTS_IN_TIME = 5;
+	
+	/**
+	 * Timeframe for MAX_RECONNECTS_IN_TIME
+	 */
+	private static final long MAX_RECONNECT_SECONDS = TimeUnit.MINUTES.toMillis(10);
 	
 	/**
 	 * The API broker
@@ -25,10 +35,16 @@ class HeartbeatThread extends ExceptionSafeThread {
 	private final BitfinexApiBroker bitfinexApiBroker;
 
 	/**
+	 * The reconnect times
+	 */
+	private final List<Long> reconnectTimes;
+	
+	/**
 	 * @param bitfinexApiBroker
 	 */
 	HeartbeatThread(BitfinexApiBroker bitfinexApiBroker) {
 		this.bitfinexApiBroker = bitfinexApiBroker;
+		this.reconnectTimes = new ArrayList<>();
 	}
 
 	@Override
@@ -48,15 +64,21 @@ class HeartbeatThread extends ExceptionSafeThread {
 				if(heartbeatTimeout < System.currentTimeMillis()) {
 					BitfinexApiBroker.logger.error("Heartbeat timeout reconnecting");
 					
+					waitForReconnectTimeslot();
+					
+					// Store the reconnect time to prevent to much
+					// reconnects in a short timeframe. Otherwise the
+					// rate limit will apply and the reconnects are not successfully
+					final long currentTime = System.currentTimeMillis();
+					reconnectTimes.removeIf((t) -> t + TimeUnit.MINUTES.toMillis(20) < currentTime);
+					reconnectTimes.add(currentTime);
+					
 					// Disable auto reconnect to ignore session closed 
 					// events, and preventing duplicate reconnects
 					bitfinexApiBroker.setAutoReconnectEnabled(false);
-					final boolean reconnectState = bitfinexApiBroker.reconnect();
+					bitfinexApiBroker.reconnect();
 					bitfinexApiBroker.setAutoReconnectEnabled(true);
-					
-					waitIfReconnectFailed(reconnectState);
 				}
-				
 			}
 			
 			try {
@@ -72,15 +94,25 @@ class HeartbeatThread extends ExceptionSafeThread {
 	 * Wait some time before the next reconnect is executed
 	 * @param reconnectState
 	 */
-	private void waitIfReconnectFailed(final boolean reconnectState) {
-		if(! reconnectState == false) {
-			// Wait some time for the reconnect
-			try {
-				Thread.sleep(TimeUnit.SECONDS.toMillis(10));
-			} catch (InterruptedException e1) {
-				Thread.currentThread().interrupt();
+	private void waitForReconnectTimeslot() {
+		
+		while(true) {
+			final long connectionsTimeframe 
+				= System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(MAX_RECONNECT_SECONDS);
+			
+			final long connectionsInTimeframe = reconnectTimes.stream().filter(t -> t > connectionsTimeframe).count();
+	
+			if(connectionsInTimeframe > MAX_RECONNECTS_IN_TIME) {
+				try {
+					Thread.sleep(TimeUnit.SECONDS.toMillis(10));
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+					return;
+				}
+			} else {
+				return;
 			}
-		}
+		} 
 	}
 	
 }
