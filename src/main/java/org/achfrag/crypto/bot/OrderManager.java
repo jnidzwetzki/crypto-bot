3c58b8a9beb46665880dd5e83b7087f22c99bfaf
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-12-04 11:52:30 +0100

    Write exchange orders to database

--- src/main/java/org/achfrag/crypto/bot/OrderManager.java
+++ src/main/java/org/achfrag/crypto/bot/OrderManager.java
@@ -3,23 +3,57 @@ package org.achfrag.crypto.bot;
 import org.achfrag.crypto.bitfinex.BitfinexApiBroker;
 import org.achfrag.crypto.bitfinex.entity.APIException;
 import org.achfrag.crypto.bitfinex.entity.BitfinexOrder;
+import org.achfrag.crypto.bitfinex.entity.ExchangeOrder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OrderManager {
 
+	/**
+	 * The bitfinex API
+	 */
 	private BitfinexApiBroker bitfinexApiBroker;
-	
+
+	/**
+	 * The session factory for persistence
+	 */
+	private final SessionFactory sessionFactory;
+
 	/**
 	 * The Logger
 	 */
 	private final static Logger logger = LoggerFactory.getLogger(OrderManager.class);
 
-
 	public OrderManager(final BitfinexApiBroker bitfinexApiBroker) {
 		this.bitfinexApiBroker = bitfinexApiBroker;
+		
+		// Persistence
+		this.sessionFactory = new Configuration().configure().buildSessionFactory();
+
+		// Register order callbacks
+		bitfinexApiBroker.addOrderCallback((o) -> handleOrderCallback(o));
 	}
 	
+	/**
+	 * Handle a new order callback
+	 * @param o
+	 * @return
+	 */
+	private void handleOrderCallback(final ExchangeOrder order) {
+		final Session session = sessionFactory.openSession();
+
+		session.beginTransaction();
+		session.save(order);
+		session.getTransaction().commit();
+	}
+
+	/**
+	 * Execute a new order
+	 * @param order
+	 */
 	public void executeOrder(final BitfinexOrder order) {
 
 		if(! bitfinexApiBroker.isAuthenticated()) {
