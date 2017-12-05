82b215381ab68ce1a7eabe80668c86fe482e4e4b
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-12-05 11:51:56 +0100

    Ported to hibernate 5

--- src/main/java/org/achfrag/crypto/bitfinex/util/HibernateUtil.java
+++ src/main/java/org/achfrag/crypto/bitfinex/util/HibernateUtil.java
@@ -1,21 +1,14 @@
 package org.achfrag.crypto.bitfinex.util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
 	private static SessionFactory sessionFactory;
 
 	public static SessionFactory getSessionFactory() {
 		if (sessionFactory == null) {
-			final Configuration configuration = new Configuration().configure();
-			final ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-					.applySettings(configuration.getProperties()).build();
-
-			// builds a session factory from the service registry
-			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			sessionFactory = new Configuration().configure().buildSessionFactory();
 		}
 
 		return sessionFactory;
