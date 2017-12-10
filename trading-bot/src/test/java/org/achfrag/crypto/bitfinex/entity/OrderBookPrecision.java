5ca6a4bffb49f0043318542f9463b02c963810f7
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-12-10 08:50:12 +0100

    Modulize

--- trading-bot/src/test/java/org/achfrag/crypto/bitfinex/entity/OrderBookPrecision.java
+++
@@ -1,8 +0,0 @@
-package org.achfrag.crypto.bitfinex.entity;
-
-public enum OrderBookPrecision {
-	P0,
-	P1,
-	P2,
-	P3;
-}
--- trading-bot/src/test/java/org/achfrag/crypto/strategy/TradeStrategyFactory.java
+++
@@ -1,9 +0,0 @@
-package org.achfrag.crypto.strategy;
-
-import org.ta4j.core.Strategy;
-import org.ta4j.core.TimeSeries;
-
-public interface TradeStrategyFactory {
-	public Strategy getStrategy(TimeSeries timeSeries);
-	public String getName();
-}
