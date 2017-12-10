891a96a6b1f889c982ab30c815ff78ae761a817b
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-12-10 09:42:53 +0100

    Refactored to app root

--- utils/count_lines.sh
+++ utils/count_lines.sh
@@ -4,18 +4,15 @@
 ######################
 
 # Is the environment configured?
-if [ -z "$BBOXDB_HOME" ]; then
-   echo "Your environment variable \$(BBOXDB_HOME) is empty. Please check your .bboxdbrc"
-   exit -1
-fi
+APP_ROOT=$(dirname $(dirname $(readlink -fm $0)))
 
 # Lines of code
-loc_java=$( (find $BBOXDB_HOME -name '*.java' -print0 | xargs -0 cat ) | wc -l)
-loc_shell=$( (find $BBOXDB_HOME -name '*.sh' -print0 | xargs -0 cat ) | wc -l)
-loc_python=$( (find $BBOXDB_HOME -name '*.py' -print0 | xargs -0 cat ) | wc -l)
-loc_xml=$( (find $BBOXDB_HOME -name '*.xml' -print0 | xargs -0 cat ) | wc -l)
-loc_yaml=$( (find $BBOXDB_HOME -name '*.yaml' -print0 | xargs -0 cat ) | wc -l)
-loc_markdown=$( (find $BBOXDB_HOME -name '*.md' -print0 | xargs -0 cat ) | wc -l)
+loc_java=$( (find $APP_ROOT -name '*.java' -print0 | xargs -0 cat ) | wc -l)
+loc_shell=$( (find $APP_ROOT -name '*.sh' -print0 | xargs -0 cat ) | wc -l)
+loc_python=$( (find $APP_ROOT -name '*.py' -print0 | xargs -0 cat ) | wc -l)
+loc_xml=$( (find $APP_ROOT -name '*.xml' -print0 | xargs -0 cat ) | wc -l)
+loc_yaml=$( (find $APP_ROOT -name '*.yaml' -print0 | xargs -0 cat ) | wc -l)
+loc_markdown=$( (find $APP_ROOT -name '*.md' -print0 | xargs -0 cat ) | wc -l)
 
 loc=$(($loc_java + $loc_shell + $loc_python + $loc_xml + $loc_yaml + $loc_markdown))
 
