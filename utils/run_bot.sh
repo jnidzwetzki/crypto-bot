c803da741c49245a3b4540981cf4bb9c56768bfc
Jan Nidzwetzki <kn@achfrag.net>
2017-12-14 21:05:02 +0100

    Changed execution

--- utils/run_bot.sh
+++ utils/run_bot.sh
@@ -3,6 +3,8 @@
 mvn install -DskipTests
 
 libs=$(find target/lib -name '*.jar' | xargs echo | tr ' ' ':')
-classpath="$libs"
+projects=$(find crypto-bot* -name '*.jar' | xargs echo | tr ' ' ':')
+
+classpath="$libs:$projects"
 
 java -cp $classpath org.achfrag.crypto.bot.DonchianBot
