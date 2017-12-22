3901dbcb48775cf6c2df5a848bb9518eef7c9310
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-12-22 17:05:31 +0100

    Changed imports

--- crypto-bot-bitfinex-api/src/main/java/net/achfrag/trading/crypto/bitfinex/OrderManager.java
+++ crypto-bot-bitfinex-api/src/main/java/net/achfrag/trading/crypto/bitfinex/OrderManager.java
@@ -3,7 +3,6 @@ package net.achfrag.trading.crypto.bitfinex;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
